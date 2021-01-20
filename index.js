23935f80fb2c93f8f8f154b28432633dee8e4729
Relten98 <diezhandinoty@gmail.com>
2021-01-19 16:16:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 // array of questions for user
 let questions = [
-
+    {
+        type: "input",
+        name: "Title_of_project",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "Who made this project?",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What is the liscence for this project?",
+    },
+    {
+        type: "input",
+        name: "Features",
+        message: "What does the project do?",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "how do you install the project?",
+    },
     
 ];
 
@@ -8,6 +32,7 @@ console.log(questions)
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
